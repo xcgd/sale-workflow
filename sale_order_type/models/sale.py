6120a1b5823e9b6c78c224c96614b86bd5eec5f5
Etienne Ferriere <eferriere@xcglpt039.local>
2021-01-27 18:00:17 +0100

    sale_order_type: Allow default types
    This allows an action / menu command that displays contextualized sales
        orders with their default types.

--- sale_order_type/models/sale.py
+++ sale_order_type/models/sale.py
@@ -29,9 +29,20 @@ class SaleOrder(models.Model):
     @api.model
     def _default_type_id(self):
         company_id = self.env.context.get("force_company", self.env.company.id)
-        return self.env["sale.order.type"].search(
+        return self.load_default_type_id() or self.env[
+            "sale.order.type"
+        ].search(
             [("company_id", "in", [company_id, False])], limit=1
         )
+
+    @api.model
+    def load_default_type_id(self):
+        default_type_id = self.env.context.get("default_type_id")
+        if default_type_id:
+            type_id = self.env["sale.order.type"].browse(default_type_id)
+        else:
+            type_id = False
+        return type_id
 
     @api.model
     def _default_sequence_id(self):
@@ -49,9 +60,13 @@ class SaleOrder(models.Model):
     @api.depends("partner_id", "company_id")
     def _compute_sale_type_id(self):
         for record in self:
+            default_sale_type = self.load_default_type_id()
             if not record.partner_id:
-                record.type_id = self.env["sale.order.type"].search(
-                    [("company_id", "in", [record.company_id.id, False])], limit=1
+                record.type_id = default_sale_type or self.env[
+                    "sale.order.type"
+                ].search(
+                    [("company_id", "in", [record.company_id.id, False])],
+                    limit=1,
                 )
             else:
                 sale_type = (
@@ -61,6 +76,7 @@ class SaleOrder(models.Model):
                     or record.partner_id.commercial_partner_id.with_context(
                         force_company=record.company_id.id
                     ).sale_type
+                    or default_sale_type
                 )
                 if sale_type:
                     record.type_id = sale_type
