21ce130519a5523cbd42803be8ea6c8613986dcd
Houzéfa Abbasbhay <houzefa.abba@xcg-consulting.fr>
2023-01-16 18:05:27 +0100

    Revert "sale_order_type: No type changes once validated"
    This reverts commit af55215921d92186cbb84052e3e18def42704596.
    
    That cset is a wrong cherry-pick of a change done in the 11.0 branch:
    6dd16edf3a1ad94811de715763240d36180bd605
    
    That feature has since been implemented in OCA's 13.0 branch; in
    particular, bebc14fb8d463a21d25aff2b67da0c266ccea5b2 fixes type
    propagation to invoices in some cases which this cherry-pick then
    reverts.

--- sale_order_type/models/account_move.py
+++ sale_order_type/models/account_move.py
@@ -16,8 +16,8 @@ class AccountMove(models.Model):
         string="Sale Type",
         compute="_compute_sale_type_id",
         store=True,
-        readonly=True,
-        states={'draft': [('readonly', False)]},
+        readonly=False,
+        states={"posted": [("readonly", True)], "cancel": [("readonly", True)]},
         copy=True,
     )
 
